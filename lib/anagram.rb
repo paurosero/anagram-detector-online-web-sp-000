cfedb8796c5518b187e25754bda823a126e4ee11
Paul Rosero <paul_rosero@yahoo.com>
2019-05-10 00:38:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ attr_accessor :name
 
   def match(words_array)
    # binding.pry
-    words_array.select {|word| word.split("")
+    words_array.select {|word| word.split("").sort == name.split("").sort}
     
     end
 
